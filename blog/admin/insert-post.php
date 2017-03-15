37a005161b662235c90ef159ca761d1f646b3405
hectorbenitez <hector.benitez.fletes@gmail.com>
2017-03-15 16:50:09 -0500

    Video 39 BLOG Saving Posts

--- blog/admin/insert-post.php
+++ blog/admin/insert-post.php
@@ -1,5 +1,15 @@
 <?php
 include_once '../config.php';
+$result = false;
+
+if (!empty($_POST)) {
+    $sql = 'INSERT INTO blog_posts (title, content) VALUES (:title, :content)';
+    $query = $pdo->prepare($sql);
+    $result = $query->execute([
+        'title' => $_POST['title'],
+        'content' => $_POST['content']
+    ]);
+}
 
 ?>
 <html>
@@ -18,8 +28,14 @@ include_once '../config.php';
     <div class="row">
         <div class="col-md-8" >
             <h2>New Post</h2>
-            <a class="btn btn-default" href="posts.php">Back</a>
-
+            <p>
+                <a class="btn btn-default" href="posts.php">Back</a>
+            </p>
+            <?php
+                if($result) {
+                    echo '<div class="alert alert-success">Post Saved!</div>';
+                }
+            ?>
             <form action="insert-post.php" method="post">
                 <div class="form-group">
                     <label for="inputTitle">Title</label>
