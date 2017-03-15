ee7339dcc0411c47e6b7b36ce1c6b2c75ee65eb3
hectorbenitez <hector.benitez.fletes@gmail.com>
2017-03-15 16:37:50 -0500

    Video 38 BLOG Admin Panel

--- blog/admin/insert-post.php
+++ blog/admin/insert-post.php
@@ -1,9 +1,6 @@
 <?php
-include_once 'config.php';
-$query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
-$query->execute();
+include_once '../config.php';
 
-$blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <html>
 <head>
@@ -20,21 +17,18 @@ $blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
     </div>
     <div class="row">
         <div class="col-md-8" >
-            <?php
-            foreach ($blogPosts as $blogPost) {
-                echo '<div class="blog-post">';
-                echo '<h2>' . $blogPost['title'] . '</h2>';
-                echo '<p>Jan 1, 2020 by <a href="">Alex</a></p>';
-                echo '<div class="blog-post-image">';
-                echo '<img src="images/keyboard.jpg" alt="">';
-                echo '</div>';
-                echo '<div class="blog-post-content">';
-                echo $blogPost['content'];
-                echo '</div>';
-                echo '</div>';
-            }
-            ?>
+            <h2>New Post</h2>
+            <a class="btn btn-default" href="posts.php">Back</a>
 
+            <form action="insert-post.php" method="post">
+                <div class="form-group">
+                    <label for="inputTitle">Title</label>
+                    <input type="text" class="form-control" name="title" id="inpuTitle">
+                </div>
+                <textarea class="form-control" name="content" id="inputContent" rows="5"></textarea>
+                <br>
+                <input class="btn btn-primary" type="submit" value="Save">
+            </form>
         </div>
         <div class="col-md-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
