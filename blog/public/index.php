d20a381fc34e48f6d32357a2587b07bc1f0eaf6b
hectorbenitez <hector.benitez.fletes@gmail.com>
2017-03-15 18:06:27 -0500

    Video 43 BLOG Router Helpers

--- blog/public/index.php
+++ blog/public/index.php
@@ -4,10 +4,20 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once '../vendor/autoload.php';
-
 include_once '../config.php';
 
+$baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+$baseUrl = 'http://' . $_SERVER['HTTP_HOST'] . $baseDir;
+define('BASE_URL', $baseUrl);
+
 $route = $_GET['route'] ?? '/';
+
+function render($fileName, $params = []) {
+    ob_start();
+    extract($params);
+    include $fileName;
+    return ob_get_clean();
+}
 
 use Phroute\Phroute\RouteCollector;
 
@@ -17,7 +27,7 @@ $router->get('/', function () use ($pdo) {
     $query->execute();
 
     $blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
-    include '../views/index.php';
+    return render('../views/index.php', ['blogPosts' => $blogPosts]);
 });
 
 $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
