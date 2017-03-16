2d52df2b14f8ad5f06d04396c4512f69d932674d
hectorbenitez <hector.benitez.fletes@gmail.com>
2017-03-16 17:36:11 -0500

    Video 54 BLOG Create User

--- blog/app/controllers/admin/UserController.php
+++ blog/app/controllers/admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 use App\Models\User;
+use Sirius\Validation\Validator;
 
 class UserController extends BaseController {
 
@@ -11,6 +12,38 @@ class UserController extends BaseController {
         $users = User::all();
         return $this->render('admin/users.twig', [
             'users' => $users
+        ]);
+    }
+
+    public function getCreate() {
+        return $this->render('admin/insert-user.twig');
+    }
+
+    public function postCreate() {
+        $errors = [];
+        $result = false;
+
+        $validator = new Validator();
+        $validator->add('name', 'required');
+        $validator->add('email', 'required');
+        $validator->add('email', 'email');
+        $validator->add('password', 'required');
+
+        if($validator->validate($_POST)) {
+            $user = new User();
+            $user->name = $_POST['name'];
+            $user->email = $_POST['email'];
+            $user->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+            $user->save();
+            $result = true;
+        } else {
+            $errors = $validator->getMessages();
+        }
+
+        return $this->render('admin/insert-user.twig', [
+            'result' => $result,
+            'errors' => $errors
         ]);
     }
 
