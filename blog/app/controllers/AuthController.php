d522ffde56fdeffe249675f81851ac2221687fbf
hectorbenitez <hector.benitez.fletes@gmail.com>
2017-03-16 18:01:59 -0500

    Video 55 BLOG Login

---
+++ blog/app/controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\User;
+use Sirius\Validation\Validator;
+
+class AuthController extends BaseController {
+
+    public function getLogin() {
+        return $this->render('login.twig');
+    }
+
+    public function postLogin() {
+        $validator = new Validator();
+        $validator->add('email', 'required');
+        $validator->add('email', 'email');
+        $validator->add('password', 'required');
+
+        if ($validator->validate($_POST)) {
+            $user = User::where('email', $_POST['email'])->first();
+            if ($user) {
+                if (password_verify($_POST['password'], $user->password)) {
+                    $_SESSION['userId'] = $user->id;
+                    header('Location:' . BASE_URL . 'admin');
+                    return null;
+                }
+            }
+
+            $validator->addMessage('email', 'Username and/or password does not match');
+        }
+
+        $errors = $validator->getMessages();
+
+        return $this->render('login.twig', [
+            'errors' => $errors
+        ]);
+    }
+}
--- blog/public/index.php
+++ blog/public/index.php
@@ -5,6 +5,8 @@ error_reporting(E_ALL);
 
 require_once '../vendor/autoload.php';
 
+session_start();
+
 $baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 $baseUrl = 'http://' . $_SERVER['HTTP_HOST'] . $baseDir;
 define('BASE_URL', $baseUrl);
@@ -35,6 +37,7 @@ use Phroute\Phroute\RouteCollector;
 
 $router = new RouteCollector();
 
+$router->controller('/auth', App\Controllers\AuthController::class);
 $router->controller('/admin', App\Controllers\Admin\IndexController::class);
 $router->controller('/admin/posts', App\Controllers\Admin\PostController::class);
 $router->controller('/admin/users', App\Controllers\Admin\UserController::class);
