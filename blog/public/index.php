e97f02a8fc37aaebd595b6906aeda991a70310a3
hectorbenitez <hector.benitez.fletes@gmail.com>
2017-03-17 10:09:33 -0500

    Video 57 BLOG Filtros

--- blog/public/index.php
+++ blog/public/index.php
@@ -37,10 +37,20 @@ use Phroute\Phroute\RouteCollector;
 
 $router = new RouteCollector();
 
+$router->filter('auth', function () {
+   if (!isset($_SESSION['userId'])) {
+       header('Location: ' . BASE_URL . 'auth/login');
+       return false;
+   }
+});
+
 $router->controller('/auth', App\Controllers\AuthController::class);
-$router->controller('/admin', App\Controllers\Admin\IndexController::class);
-$router->controller('/admin/posts', App\Controllers\Admin\PostController::class);
-$router->controller('/admin/users', App\Controllers\Admin\UserController::class);
+
+$router->group(['before' => 'auth'], function ($router) {
+    $router->controller('/admin', App\Controllers\Admin\IndexController::class);
+    $router->controller('/admin/posts', App\Controllers\Admin\PostController::class);
+    $router->controller('/admin/users', App\Controllers\Admin\UserController::class);
+});
 $router->controller('/', App\Controllers\IndexController::class);
 
 $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
